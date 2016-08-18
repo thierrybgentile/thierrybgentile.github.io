26edb9dfd10524807c895ebd2c4214dabc303114
thierrybgentile <thierry.bgentile@gmail.com>
2016-08-18 09:37:00 -0400

    Black button nav

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,15 @@
 var menuButton = document.getElementById('menu-button');
-var menu = document.querySelector('nav')
+var menu = document.querySelector('nav');
+var logo = document.querySelector('.logo');
+
 menuButton.addEventListener("click", toggleMenu);
+logo.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
+  if (menuButton.style.transform === "rotate(45deg)") {
+    menuButton.style.transform = "";
+  } else {
+        menuButton.style.transform = "rotate(45deg)";
+  }
   menu.classList.toggle('hide');
 }
